8468845c3048f1b5001c71d90730525484ee45f4
berdimuratgafurov251-blip <berdimuratgafurov251@gmail.com>
2026-04-21 12:55:52 +0500

    Update app.py

--- faq_rag_chatbot/app.py
+++ faq_rag_chatbot/app.py
@@ -194,7 +194,7 @@ Answer:
 """
 
     with st.spinner("Thinking..."):
-       response = model.generate_content(prompt)
+       response = model.generate_content("gemini-1.5-flash-latest")
 
     answer = response.text if response.text else "No response"
 
