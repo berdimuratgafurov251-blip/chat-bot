adbf6d75bbb0229bb413938c3402bffc82418e1c
berdimuratgafurov251-blip <berdimuratgafurov251@gmail.com>
2026-04-21 13:55:17 +0500

    Update app.py

--- faq_rag_chatbot/app.py
+++ faq_rag_chatbot/app.py
@@ -199,7 +199,7 @@ Answer:
 
     with st.spinner("Thinking..."):
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-2.5-flash",
             contents=[prompt]
 )
 
