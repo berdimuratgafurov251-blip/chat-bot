0f6d9884a67a469fbd82630c0f7ca530aa49412c
berdimuratgafurov251-blip <berdimuratgafurov251@gmail.com>
2026-05-03 12:50:34 +0500

    Update ingest.py

--- faq_rag_chatbot/ingest.py
+++ faq_rag_chatbot/ingest.py
@@ -4,31 +4,14 @@ from utils import split_text
 def load_file(file):
     filename = file.name.lower()
 
-    text = ""
-    total_chunks = 0
-
     if filename.endswith(".txt"):
 
-        buffer = ""
+        text = file.read().decode("utf-8", errors="ignore")
 
-        for line in file:
-            try:
-                buffer += line.decode("utf-8")
-            except:
-                continue
+        chunks = split_text(text, chunk_size=300)
 
-            if len(buffer) > 3000:
-                chunks = split_text(buffer, chunk_size=300)
-                add_to_index(chunks)
-                total_chunks += len(chunks)
-                buffer = ""
-
-        if buffer:
-            chunks = split_text(buffer, chunk_size=300)
+        if chunks:
             add_to_index(chunks)
-            total_chunks += len(chunks)
+            return len(chunks)
 
-    else:
-        return 0
-
-    return total_chunks
+    return 0
