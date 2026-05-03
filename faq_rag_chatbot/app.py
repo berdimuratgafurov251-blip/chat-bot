fc710bca6c3ef2d09e1e7be10d3d33c58f2f9f56
berdimuratgafurov251-blip <berdimuratgafurov251@gmail.com>
2026-05-03 13:56:39 +0500

    Update app.py

--- faq_rag_chatbot/app.py
+++ faq_rag_chatbot/app.py
@@ -186,7 +186,6 @@ else:
     st.sidebar.info("Guest mode")
 
 # ================= FILE UPLOAD =================
-st.set_option('server.maxUploadSize', 1000)
 def save_uploaded_file(uploaded_file):
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
         tmp.write(uploaded_file.getbuffer())
